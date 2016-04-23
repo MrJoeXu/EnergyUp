1c21fa21462d04cd83a3d12c8728e53f64049ec8
MrJoeXu <zxu3@wpi.edu>
2016-04-23 16:09:49 -0400

    change Manifest Config

--- app/src/main/java/lxx_team/energyup/EnergyUp.java
+++ app/src/main/java/lxx_team/energyup/EnergyUp.java
@@ -12,12 +12,13 @@ import com.avos.avoscloud.AVOSCloud;
  /**
  * Created by JoeXu on 4/23/16.
  */
-public class MyLeanCloudApp extends Application{
+public class EnergyUp extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
 
         // Initialize parameter for this, AppId, AppKey
         AVOSCloud.initialize(this, "3z95KghEnMhAKeF96q0btdQ3-MdYXbMMI", "tV0H3rwu8S4dvXiuFd9XC0y6");
+        AVOSCloud.useAVCloudUS();
     }
 }
