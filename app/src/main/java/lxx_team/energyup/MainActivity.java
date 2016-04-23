d993ded78108d3ae22fcb8e6fac23a9d648ffb19
MrJoeXu <zxu3@wpi.edu>
2016-04-23 16:16:14 -0400

    add testing for LeanCloud

--- app/src/main/java/lxx_team/energyup/MainActivity.java
+++ app/src/main/java/lxx_team/energyup/MainActivity.java
@@ -2,14 +2,33 @@ package lxx_team.energyup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVObject;
+import com.avos.avoscloud.SaveCallback;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 测试 SDK 是否正常工作的代码
+        AVObject testObject = new AVObject("TestObject");
+        testObject.put("words", "Hello World!");
+        testObject.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(AVException e) {
+                if (e == null) {
+                    Log.d("saved", "success!");
+                }
+            }
+        });
+
         setContentView(R.layout.activity_main);
     }
 
