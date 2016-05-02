e22fd384e76c82b6e86c35a07dcb363023ef2998
steveyxu <xy578772313@gmail.com>
2016-05-02 10:27:18 -0400

    bbb

--- app/src/main/java/lxx_team/energyup/QRCodeActivity.java
+++ app/src/main/java/lxx_team/energyup/QRCodeActivity.java
@@ -1,6 +1,7 @@
 package lxx_team.energyup;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,24 +13,34 @@ import android.widget.Toast;
 import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVObject;
 import com.avos.avoscloud.AVQuery;
+import com.avos.avoscloud.AVUser;
 import com.avos.avoscloud.GetCallback;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
+import com.tencent.qc.stat.common.User;
 
 import static com.google.zxing.BarcodeFormat.*;
 
 public class QRCodeActivity extends Activity {
     private ImageView imageView;
-    Bitmap bitmap;
+    private Bitmap bitmap;
+    private String uname;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode);
 
-        String uname = "kkk";
+        AVUser currentUser = AVUser.getCurrentUser();
+        if (currentUser != null) {
+           uname = currentUser.getEmail();
+        }
+        else {
+            Intent i = new Intent(QRCodeActivity.this, UserLogin.class);
+            startActivity(i);
+        }
 
         MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
         try {
